23ed49b9dc9ba8a0a00ddd2be8782940ad1e772e
kam.chander@bionic.co.uk <kam.chander@bionic.co.uk>
2023-08-04 23:34:50 +0000

    SCD Type 2 Snapshot

---
+++ snapshots/scd2_FactOrders.sql
@@ -0,0 +1,18 @@
+{% snapshot FactOrders %}
+
+    {% set new_schema = target.schema + "_snapshot" %}
+
+    {{
+        config(
+            target_database="Analytics",
+            target_schema=new_schema,
+            unique_key="orderid",
+            strategy="timestamp",
+            updated_at="OrderUpdatedDate",
+        )
+    }}
+
+    select *
+    from analytics.{{ target.schema }}.FactOrders
+
+{% endsnapshot %}
