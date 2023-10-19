d62a80659a2cdcc91d77bf4fc6d69948165d2fb1
kam.chander@bionic.co.uk <kam.chander@bionic.co.uk>
2023-10-19 10:49:32 +0000

    save

--- snapshots/scd2_FactOrders.sql
+++ snapshots/scd2_FactOrders.sql
@@ -1,17 +1,14 @@
---- To check check all columns for changes use 'all'
--- To check one than one columns for changes use ['column1','column2']
---updated_at="OrderUpdatedDate", 
 {% snapshot FactOrders %}
 
     {% set new_schema = target.schema + "_snapshot" %}
 
     {{
         config(
-            target_database="Analytics",
+            target_database="analytics",
             target_schema=new_schema,
             unique_key="orderid",
             strategy="timestamp",
-            updated_at="OrderUpdatedDate"
+            updated_at="OrderUpdatedDate",
             
         )
     }}
